e7010a9b25db11ad77935be89de00110fe742112
AryaVeerSahni <f20250626@pilani.bits-pilani.ac.in>
2026-04-10 15:06:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const corner4 = document.querySelectorAll(`.corner`)[3];
 const shadowDivJs = document.querySelector('#shadowDivId');
 const detailsSecJs = document.querySelector('#detailsSecId');
 const costaaImgJs = document.querySelector('.costaaImg');
+const deptDivJs = document.querySelector('#deptDivId');
 
-const deptDivJs = document.querySelector('.deptDiv');
-const Lines = document.querySelectorAll('.upLine');
+const line1 = document.querySelector('#upline');
+const line2 = document.querySelector('#downline');
 
 const costaaGlitch = PowerGlitch.glitch(costaaImgJs, {
     playMode: "manual"
@@ -60,10 +61,8 @@ function everything(name, depName, imgPath) {
     
     setTimeout(()=>{
         costaaGlitch.stopGlitch();
-        detailsSecJs.innerHTML = `<div class="nameDiv">${name}</div>
-                <span class="upLine"></span>
-                <div class="deptDiv">${depName}</div>
-                <span class="upLine"></span>`
+        detailsSecJs.querySelector('.nameDiv').innerHTML = name;
+        detailsSecJs.querySelector('.deptDiv').innerHTML = depName;
     }, 2500)
 
     setTimeout(()=>{
@@ -110,42 +109,88 @@ function everything(name, depName, imgPath) {
     }, 3500)
 }
 
+function photoAppear() {
+    setTimeout(()=>{
+        shadowDivJs.classList.remove('disNone');
+        line1.classList.remove('disNone');
+        line2.classList.remove('disNone');
+        detailsSecJs.classList.remove('detailsSecPosn1');
+        detailsSecJs.classList.add('detailsSecPosn2');
+    }, 1500)
+}
+
 document.addEventListener('keydown', event=>{
+    if(event.key === '0'){
+        everything(`Welcome To<br><br><span id="apogee">APOGEE</span>`, ``, `sajalBhaiya.png`);
+        setTimeout(()=>{
+            shadowDivJs.classList.add('disNone');
+            line1.classList.add('disNone');
+            line2.classList.add('disNone');
+            detailsSecJs.classList.remove('detailsSecPosn2');
+            detailsSecJs.classList.add('detailsSecPosn1');
+        }, 1500)
+    }
     if(event.key === '1'){
         everything(`Atharv<br>Agarwal`, `Department of Visual Media`, `atharvBhaiya.png`);
-        Lines.classList.remove('disNone');
-        detailsSecJs.style.top = '12vh';
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv dvm`;
+        }, 1500)
+    }
+    if(event.key === '2'){
+        everything(`Ishita<br>Sethi`, `Department of Paper<br>Evaluation and Presentation`, `ishitaDidi.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv pep`;
+        }, 1500)
+    }
+    if(event.key === '3'){
+        everything(`Aurvind<br>Mohanty`, `Department of Art,<br>Design and Publicity`, `aurvindBhaiya.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv adp`;
+        }, 1500)
     }
     if(event.key === '4'){
-        everything(`Anagha<br>Sharma`, `Department of<br>Publications and<br>Correspondence<br>for APOGEE`, `anaghaDidi.png`);
-        Lines.classList.remove('disNone');
+        everything(`Anagha<br>Sharma`, `Department of Publications and<br>Correspondence for APOGEE`, `anaghaDidi.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv pcra`;
+        }, 1500)
     }
     if(event.key === '5'){
         everything(`Shreyak<br>Shintre`, `Department of Controls`, `shreyakBhaiya.png`);
-        Lines.classList.remove('disNone');
-    }
-    if(event.key === '7'){
-        everything(`Shivansh<br>Rastogi`, `Department of<br>Sponsorship and<br>Marketing`, `shivanshBhaiya.png`);
-        Lines.classList.remove('disNone');
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv controls`;
+        }, 1500)
     }
     if(event.key === '6'){
-        everything(`Shreya<br>Karnwal`, `Department of<br>Reception and<br>Accommodation`, `shreyaDidi.png`);
-        Lines.classList.remove('disNone');
+        everything(`Shreya<br>Karnwal`, `Department of Reception<br>and Accommodation`, `shreyaDidi.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv recnacc`;
+        }, 1500)
     }
-    if(event.key === '3'){
-        everything(`Aurvind<br>Mohanty`, `Department of<br>Art, Design<br>and Publicity`, `aurvindBhaiya.png`);
-        Lines.classList.remove('disNone');
-    }
-    if(event.key === '2'){
-        everything(`Ishita<br>Sethi`, `Department of<br>Paper Evaluation<br>and Presentation`, `ishitaDidi.png`);
-        Lines.classList.remove('disNone');
+    if(event.key === '7'){
+        everything(`Shivansh<br>Rastogi`, `Department of Sponsorship<br>and Marketing`, `shivanshBhaiya.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv spons`;
+        }, 1500)
     }
     if(event.key === '8'){
         everything(`Aditya<br>Khandelwal`, `General Secretary,<br>Students' Union`, `adityaBhaiya.png`);
-        Lines.classList.remove('disNone');
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv gensec`;
+        }, 1500)
     }
     if(event.key === '9'){
-        everything(`Sajal<br>Yadav`, `President,<br>Students' Union`, `sajalBhaiya.png`);
-        Lines.classList.remove('disNone');
+        everything(`Sajal Yadav`, `President,<br>Students' Union`, `sajalBhaiya.png`);
+        photoAppear();
+        setTimeout(()=>{
+            deptDivJs.className = `deptDiv prez`;
+        }, 1500)
     }
 })
